72826868703ab0413cf9621c88a192ef84e9e790
FerrusManus <roma.hlinin@yandex.ru>
2023-04-27 18:24:47 +0300

    321

--- WebApplication1/Controllers/Updates.cs
+++ WebApplication1/Controllers/Updates.cs
@@ -10,26 +10,26 @@ namespace WebApplication1.Controllers
 		/// <summary>
 		/// Тип события
 		/// </summary>
-		[JsonProperty("type")]
+		[JsonProperty("type", NullValueHandling=NullValueHandling.Ignore)]
 		public string Type { get; set; }
 
 		/// <summary>
 		/// Объект, инициировавший событие
 		/// Структура объекта зависит от типа уведомления
 		/// </summary>
-		[JsonProperty("object")]
+		[JsonProperty("object", NullValueHandling=NullValueHandling.Ignore)]
 		public JObject Object { get; set; }
 
 		/// <summary>
 		/// ID сообщества, в котором произошло событие
 		/// </summary>
-		[JsonProperty("group_id")]
+		[JsonProperty("group_id", NullValueHandling=NullValueHandling.Ignore)]
 		public long GroupId { get; set; }
 
 		/// <summary>
 		/// Секретный ключ. Передается с каждым уведомлением от сервера
 		/// </summary>
-		[JsonProperty("secret")]
+		[JsonProperty("secret", NullValueHandling=NullValueHandling.Ignore)]
 		public string Secret { get; set; }
 	}
 }
