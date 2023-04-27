b0d6706bc590f9a814632a5e8d50b474dce06885
FerrusManus <roma.hlinin@yandex.ru>
2023-04-27 18:40:44 +0300

    321

---
+++ WebApplication1/Controllers/Confirmation.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace WebApplication1.Controllers;
+
+public class Confirmation
+{
+    [JsonProperty("type")]
+    public string Type { get; set; }
+    
+    [JsonProperty("group_id")]
+    public long GroupId { get; set; }
+}
