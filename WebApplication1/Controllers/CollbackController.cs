5e5281c59a9c5b370bcacb5c76728f8c92864238
FerrusManus <roma.hlinin@yandex.ru>
2023-04-27 17:48:09 +0300

    321

--- WebApplication1/Controllers/CollbackController.cs
+++ WebApplication1/Controllers/CollbackController.cs
@@ -18,8 +18,29 @@ public class CallbackController : ControllerBase
         _vkApi = api;
     }
 
+   using Microsoft.AspNetCore.Mvc;
+using VkNet.Abstractions;
+
+namespace WebApplication1.Controllers;
+
+[Route("VkCallbacks")]
+[ApiController]
+public class CallbackController : ControllerBase
+{
+    /// <summary>
+    /// Конфигурация приложения
+    /// </summary>
+    private readonly IConfiguration _configuration;
+    private readonly IVkApi _vkApi;
+    public CallbackController(IConfiguration configuration,IVkApi api)
+    {
+        _configuration = configuration;
+        _vkApi = api;
+    }
+
     [HttpPost]
-    public IActionResult Callback([FromBody] Updates updates)
+    [Route("confirmation")]
+    public Task Callback([FromBody] Updates updates)
     {
         Console.WriteLine("fired");
         // Тип события
@@ -28,9 +49,13 @@ public class CallbackController : ControllerBase
             // Если это уведомление для подтверждения адреса
             case "confirmation":
                 // Отправляем строку для подтверждения 
-                return Ok("2644ba0e");
+                HttpContext.Response.WriteAsJsonAsync("2644ba0e");
+                
+                return Task.CompletedTask;
         }
 
-        return BadRequest();
+        return Task.CompletedTask;
     }
+}
+
 }
